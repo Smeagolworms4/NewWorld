f1c132cfcdc01df722fba1e5c4592815319a284a
SmeagolWorms4 <smeagolworms4@gmail.com>
2016-01-05 21:36:54 +0100

    Add server http

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 //require('./include.js');
 //var server = new Server();
-console.log("Hello Jordan");
+var express = require('express');
+var app = express();
+app.use('', express.static('./public'));
+console.log ('Http: Start: http://127.0.0.1:8383');
+app.listen(8383);
